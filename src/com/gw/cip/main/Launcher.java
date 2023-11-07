fa85356482a69f7e26a7e7a171d78482b4c16b49
Siddhartha Bhattacharyya <sbhattacharyya@guidewire.com>
2023-11-07 16:15:20 +0000

    added logic to generate basic xml

--- src/com/gw/cip/main/Launcher.java
+++ src/com/gw/cip/main/Launcher.java
@@ -1,15 +1,31 @@
 package com.gw.cip.main;
 
 import java.io.File;
+import java.io.OutputStream;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
+
+import org.w3c.dom.CDATASection;
+import org.w3c.dom.Comment;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class Launcher {
 
     String functionFilePath;
-    HashMap<String, String> inputParameters = new HashMap<String,String>();
+    LinkedHashMap<String, String> inputParameters = new LinkedHashMap<String,String>();
     String inputParametersAsString = "";
     String functionBody = "";
+    String unitName;
 
     public Launcher(String functionFilePath) {
         this.functionFilePath = functionFilePath;
@@ -23,7 +39,10 @@ public class Launcher {
             while(reader.hasNextLine()) {
                 String data = reader.nextLine();
                 if(data.startsWith("function")) {
-                    System.out.println(data);
+                    unitName = data.substring(data.indexOf(' '),data.indexOf('(')).trim();
+                    inputParametersAsString = data.substring(data.indexOf('(')+1,data.indexOf(')')).trim();
+                    System.out.println(unitName);
+                    System.out.println(inputParametersAsString);
                 }
             }
         }catch(Exception e) {
@@ -33,10 +52,55 @@ public class Launcher {
     }
 
     private void generateInputParameters() {
+        String[] individualInputParameters = inputParametersAsString.split(",");
+        for(int i = 0; i < individualInputParameters.length ; i++) {
+            String parameter = individualInputParameters[i].trim();
+            inputParameters.put(parameter.split(":")[0].trim(),parameter.split(":")[1].trim());
+        }
+        System.out.println("INPUt PARAMS ");
+        for(Map.Entry<String,String> entry : inputParameters.entrySet()) {
+            System.out.println(entry.getKey() + "," + entry.getValue());
+        }
+    }
+
+    private void generateExecuteFunction() throws ParserConfigurationException, TransformerException {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        Document doc = docBuilder.newDocument();
+        Element rootElement = doc.createElement("PCF");
+        doc.appendChild(rootElement);
+        Element dvPanelElement = doc.createElement("DetailViewPanel");
+        rootElement.appendChild(dvPanelElement);
+        Element inputColumnElement = doc.createElement("InputColumn");
+        dvPanelElement.appendChild(inputColumnElement);
+
+        Element textInputElement = doc.createElement("TextInput");
+        textInputElement.setAttribute("editable","true");
+        textInputElement.setAttribute("id","account");
+        textInputElement.setAttribute("label","Account");
+        textInputElement.setAttribute("value","1234");
+
+        inputColumnElement.appendChild(textInputElement);
+
+        writeXml(doc,System.out);
 
     }
 
-    private void generateExecuteFunction() {
+    private void writeXml(Document doc,
+                                 OutputStream output)
+            throws TransformerException {
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        // pretty print
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(output);
+
+        transformer.transform(source, result);
 
     }
 
@@ -45,8 +109,10 @@ public class Launcher {
 
     }
 
-    public static void main(String args []) {
+    public static void main(String args []) throws ParserConfigurationException, TransformerException {
         Launcher code2UITest = new Launcher("C:\\Code2UI\\testFunction.txt");
-        code2UITest.generateUIXML();
+        //code2UITest.generateUIXML();
+        //code2UITest.generateInputParameters();
+        code2UITest.generateExecuteFunction();
     }
 }
