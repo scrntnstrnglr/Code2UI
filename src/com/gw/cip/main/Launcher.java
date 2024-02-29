674d13931f888cf7df88b18f6e1c3fc0ce115cf4
Siddhartha Bhattacharyya <sbhattacharyya@guidewire.com>
2024-02-29 17:28:19 +0000

    Added logic for adding field attributes and values

--- src/com/gw/cip/main/Launcher.java
+++ src/com/gw/cip/main/Launcher.java
@@ -7,6 +7,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -20,6 +21,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+import com.gw.cip.main.ui.field.ToolbarButton;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -29,11 +32,8 @@ import javax.xml.transform.stream.StreamResult;
 
 public class Launcher {
 
-    String functionFilePath;
     LinkedHashMap<String, String> inputParameters = new LinkedHashMap<String,String>();
-    String inputParametersAsString = "";
-    String functionBody = "";
-    String unitName;
+    String functionFilePath, inputParametersAsString = "", functionBody = "", unitName = "", functionCallName = "";
     Properties configProperties = new Properties();
 
     public Launcher(String functionFilePath) throws FileNotFoundException, IOException {
@@ -42,20 +42,28 @@ public class Launcher {
         configProperties.load(new FileInputStream(System.getProperty("user.dir") + "\\config\\field.properties"));
     }
 
+    protected void generate() throws ParserConfigurationException, TransformerException { 
+        this.generateUIXML();
+        this.generateInputParameters();
+        this.generateFunctionCallNameFromPCF();
+        this.generateExecuteFunction();
+    }
+
     private void parseFunctionFile() {
         try{
             File functionFile = new File(functionFilePath);
             Scanner reader = new Scanner(functionFile);
 
             while(reader.hasNextLine()) {
-                String data = reader.nextLine();
+                String data = reader.nextLine() + "\n";
+                functionBody = functionBody + data;
                 if(data.startsWith("function")) {
                     unitName = data.substring(data.indexOf(' '),data.indexOf('(')).trim();
                     inputParametersAsString = data.substring(data.indexOf('(')+1,data.indexOf(')')).trim();
-                    System.out.println(unitName);
-                    System.out.println(inputParametersAsString);
                 }
             }
+            System.out.println("FUNCTION BODY : ");
+            System.out.println(functionBody);
         }catch(Exception e) {
             System.out.println(e);
             e.printStackTrace();
@@ -66,12 +74,18 @@ public class Launcher {
         String[] individualInputParameters = inputParametersAsString.split(",");
         for(int i = 0; i < individualInputParameters.length ; i++) {
             String parameter = individualInputParameters[i].trim();
-            inputParameters.put(parameter.split(":")[0].trim(),parameter.split(":")[1].trim());
+            inputParameters.put(parameter.split(":")[0].trim()+"_PCF",parameter.split(":")[1].trim());
         }
         System.out.println("INPUt PARAMS ");
         for(Map.Entry<String,String> entry : inputParameters.entrySet()) {
             System.out.println(entry.getKey() + "," + entry.getValue());
         }
+    }
+
+    private void generateFunctionCallNameFromPCF() {
+        var inputParamList = new ArrayList<String>(inputParameters.keySet());
+        var inputParamsCommaSeparated = String.join(",", inputParamList);
+        functionCallName = unitName + "(" + inputParamsCommaSeparated + ")";
     }
 
     private void generateExecuteFunction() throws ParserConfigurationException, TransformerException {
@@ -97,11 +111,13 @@ public class Launcher {
         }
 
         Element toolbarElement = doc.createElement("Toolbar");
-        rootElement.appendChild(toolbarElement);
+        screenPanelElement.appendChild(toolbarElement);
 
         Element toolbarButtonElement = doc.createElement("ToolbarButton");
-        toolbarButtonElement.setAttribute("id", "executeButton");  //add function name and function call
-        toolbarButtonElement.setAttribute("label", "&quot;Execute&quot;");
+        ToolbarButton toolbarButton = new ToolbarButton(configProperties.getProperty("ToolbarButton"),toolbarButtonElement);
+       /** toolbarButtonElement.setAttribute("id", "executeButton");  //add function name and function call
+        toolbarButtonElement.setAttribute("label", "\"Execute\"");
+        toolbarButtonElement.setAttribute("action",functionCallName ); */
         toolbarElement.appendChild(toolbarButtonElement);
 
         Element dvPanelElement = doc.createElement("DetailViewPanel");
@@ -112,28 +128,45 @@ public class Launcher {
         dvPanelElement.appendChild(inputColumnElement);
 
         for(Map.Entry<String,String> entry : inputParameters.entrySet()) {
-            System.out.println(entry.getValue());
-            System.out.println(configProperties.getProperty(entry.getValue()));
-            Element inputElement = doc.createElement(configProperties.getProperty(entry.getValue()));
-            inputElement.setAttribute("editable", "true");
-            inputElement.setAttribute("id", entry.getKey().trim());
-            //inputElement.setAttribute("label","\u0026"+entry.getKey().trim()+"\u0026");
-            inputElement.setAttribute("label","\0022" +"sdasd"+ "\0");
-            inputElement.setAttribute("value",entry.getKey());
-            inputElement.setAttribute("valueType",entry.getValue());
+            String uiInputFieldConfig = configProperties.getProperty(entry.getValue());
+            String[] uiInputFieldNameAndProperties = uiInputFieldConfig.split(",");
+            String uiInputField = uiInputFieldNameAndProperties[0];
+            Element inputElement = doc.createElement(uiInputField);
+            for(int i=1;i<uiInputFieldNameAndProperties.length;i++) {
+                String attributeName = uiInputFieldNameAndProperties[i];
+                setAttribute(attributeName,entry,inputElement);
+            }
             inputColumnElement.appendChild(inputElement);
         }
 
         Element codeElement = doc.createElement("Code");
         screenPanelElement.appendChild(codeElement);
 
-        CDATASection codeSection = doc.createCDATASection("print(\"\")");
+        CDATASection codeSection = doc.createCDATASection(functionBody);
         codeElement.appendChild(codeSection);
     
-        try (FileOutputStream output = new FileOutputStream("C:\\MWFBI\\billingcenter\\modules\\configuration\\gsrc\\pcf\\MyScreen.pcf")) {
+        try (FileOutputStream output = new FileOutputStream("C:\\MWFBI\\billingcenter\\modules\\configuration\\config\\web\\pcf\\myScreen.pcf")) {
             writeXml(doc, output);
         }catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void setAttribute(String attributeName, Map.Entry<String,String> entry, Element inputElement) {
+        switch(attributeName) {
+            case "editable" : inputElement.setAttribute(attributeName, "true");
+            break;
+            case "id" : inputElement.setAttribute(attributeName, entry.getKey().trim());
+            break;
+            case "label" : inputElement.setAttribute(attributeName, "\""+entry.getKey().trim()+"\"");
+            break;
+            case "value" : inputElement.setAttribute(attributeName, entry.getKey());
+            break;
+            case "valueType" : inputElement.setAttribute(attributeName, entry.getValue());
+            break;
+            case "currency" : inputElement.setAttribute(attributeName, "Currency.TC_USD");
+            break;
+            default : inputElement.setAttribute(attributeName, "");
         }
     }
 
@@ -159,8 +192,6 @@ public class Launcher {
 
     public static void main(String args []) throws ParserConfigurationException, TransformerException, FileNotFoundException, IOException {
         Launcher code2UITest = new Launcher("C:\\bc-ci-mvp\\Code2UI\\testFunction.txt");
-        code2UITest.generateUIXML();
-        code2UITest.generateInputParameters();
-        code2UITest.generateExecuteFunction();
+        code2UITest.generate();
     }
 }
---
+++ src/com/gw/cip/main/ui/field/ToolbarButton.java
@@ -0,0 +1,53 @@
+package com.gw.cip.main.ui.field;
+
+public class ToolbarButton extends Element{
+
+
+    public ToolbarButton(String attributeNames, org.w3c.dom.Element xmlElement) {
+        super(attributeNames,xmlElement);
+        this.loadRequiredAttributes();
+    }
+
+    @Override
+    public String getID() {
+        // TODO Auto-generated method stub
+        return "executeButton";
+    }
+
+    @Override
+    public String getLabel() {
+        // TODO Auto-generated method stub
+        return "\"Execute\"";
+    }
+
+    @Override
+    public String getValue() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getValue'");
+    }
+
+    @Override
+    public String getValueType() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getValueType'");
+    }
+
+    @Override
+    public String getEditable() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEditable'");
+    }
+
+    @Override
+    public String getCurrency() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCurrency'");
+    }
+
+    @Override
+    public String getAction() {
+        // TODO Auto-generated method stub
+        return "tesetFunctionCall()";
+    }
+    
+}
